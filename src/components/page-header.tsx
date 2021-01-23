f6ab7dd534caf0a9ad967bd85e39a80e43ae627e
Matt Deitke <mdeitke23@gmail.com>
2021-01-22 20:57:13 -0800

    update head with 2020 challenges/2021 soon

--- src/components/page-header.tsx
+++ src/components/page-header.tsx
@@ -1,39 +1,89 @@
 import React from "react";
 import style from "./page-header.module.scss";
 import { Popover, Button } from "antd";
-import { DownOutlined, LeftOutlined } from "@ant-design/icons";
+import { DownOutlined, RightOutlined } from "@ant-design/icons";
 import SeattleCover from "../../static/images/cvpr2020/cover-transparent.svg";
+import { Emoji } from "emoji-mart";
 
 // This will eventually show the 2021 challenges as a dropdown
 // on the header row. 2021 info coming soon!
 // For now, it's still in place so that the title is centered.
 const Challenges = () => (
-  <div className={style.right}>
-    {/* 
-      <Popover placement="bottomRight" content="Coming Soon!" trigger="hover">
-        <Button
-          style={{
-            background: "inherit",
-            color: "inherit",
-            border: "none",
-            boxShadow: "none",
-            padding: "0px",
-            fontSize: "15px",
-          }}
-        >
-          CVPR 2021 Challenges <DownOutlined style={{ fontSize: "14px" }} />
-        </Button>
-      </Popover>
-      */}
+  <div className={style.left}>
+    <Popover
+      placement="bottomLeft"
+      content={
+        <div>
+          <div>
+            <a
+              href="http://svl.stanford.edu/igibson/challenge.html"
+              target="_blank"
+            >
+              iGibson Challenge
+            </a>
+          </div>
+          <div>
+            <a href="https://aihabitat.org/challenge/2020/" target="_blank">
+              Habitat Challenge
+            </a>
+          </div>
+          <div>
+            <a
+              href="https://ai2thor.allenai.org/robothor/challenge"
+              target="_blank"
+            >
+              RoboTHOR Challenge
+            </a>
+          </div>
+        </div>
+      }
+      trigger="hover"
+    >
+      <Button
+        style={{
+          background: "inherit",
+          color: "inherit",
+          border: "none",
+          boxShadow: "none",
+          padding: "0px",
+          fontSize: "15px",
+        }}
+      >
+        <DownOutlined style={{ fontSize: "14px" }} /> CVPR 2020 Challenges
+      </Button>
+    </Popover>
   </div>
 );
 
 // This will eventually allow the old workshops (e.g., CVPR 2020)
 // to be accessible from the header. Waiting until 2021 info is up
 // for this to display anything.
-const LastYear = () => (
-  <div className={style.left}>
-    {/* <LeftOutlined style={{ fontSize: "14px" }} /> CVPR 2020 Workshop */}
+const NextYear = () => (
+  <div className={style.right}>
+    <Popover
+      placement="bottomRight"
+      content={
+        <>
+          <span className={style.comingSoonText}>Coming February 2021</span>{" "}
+          <Emoji emoji="face_with_cowboy_hat" set="apple" size={22} />
+        </>
+      }
+      trigger="hover"
+    >
+      <Button
+        style={{
+          background: "inherit",
+          color: "inherit",
+          border: "none",
+          boxShadow: "none",
+          padding: "0px",
+          fontSize: "15px",
+        }}
+      >
+        CVPR 2021 Workshop{" "}
+        <RightOutlined style={{ fontSize: "14px" }} className={style.emoji} />
+      </Button>
+    </Popover>
   </div>
 );
 
@@ -44,12 +94,12 @@ export const Header = (props: { conference: string }) => (
   <>
     <div className={style.header}>
       <div className={style.headerContent}>
-        <LastYear />
+        <Challenges />
         <div className={style.middle}>
           <div className={style.workshopTitle}>Embodied AI Workshop</div>
           <div className={style.conference}>{props.conference}</div>
         </div>
-        <Challenges />
+        <NextYear />
         <div
           style={{
             width: "100%",
