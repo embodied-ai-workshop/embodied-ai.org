812111ffe92a88e4df388fbc253d934a48a7cc2b
Anthony Francis <anthony@novarctech.com>
2024-06-15 17:34:24 -0700

    Added attendee information.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -751,6 +751,16 @@ export default function Home({ data }) {
           />
         }
       >
+      <Section title="Attending">
+        The Fifth Annual Embodied AI Workshop will be held 
+        <b> Tuesday, June 18 from 8:50am to 5:30pm Pacific </b> 
+        in conjunction with <a href="https://cvpr.thecvf.com/">CVPR 2024</a>.
+        <ul>
+          <li>The physical workshop will be held in Summit 428.</li>
+          <li>The workshop will also be <a href="https://cvpr.thecvf.com/virtual/2024/workshop/23598">livestreamed</a> for CVPR virtual attendees.</li>
+        </ul>
+        Please join us at Embodied AI #5!
+      </Section>
       <Section title="Overview">
         <p>
           Minds live in bodies, and bodies move through a changing world.
