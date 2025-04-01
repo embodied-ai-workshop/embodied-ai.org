b750096e4e68a7f5eeaa57e8bda1a4a87d228972
Anthony Francis <centaur@logicalrobotics.com>
2025-03-31 21:44:50 -0400

    Update website for launch.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ const IndexPage = () => {
   // Hardwire the year redirector until the new year goes live.
   // const currentYear = new Date().getFullYear();
   // const redirectYear = process.env.GATSBY_REDIRECT_YEAR || currentYear; // Default to the current year
-  const redirectYear = "2024";
+  const redirectYear = "2025";
   const targetPage = `/cvpr${redirectYear}/`;
   
   React.useEffect(() => {
